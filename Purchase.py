bf4bd1a98805f31fe3456079c6b406d3bf9ad033
Esther UGOLINI <eugoli@teaching.CS.McGill.CA>
2014-12-01 00:06:15 -0500

    yoyoyo

--- Purchase.py
+++ Purchase.py
@@ -3,6 +3,7 @@
 #TO DO: MAKE IT SO THAT THE INVENTORY DECREASES AS YOU BUY STUFF. 
 #TO DO: MAKE IT SO THAT YOU CANNOT BUY STUFF IF THERE ISN'T ENOUGH IN THE INVENTORY
 #import requests
+import os
 import re
 import cgi
 import cgitb
@@ -16,8 +17,8 @@ try:
 	inven = open("inventory.csv", "r+")
 except IOError:
 	print "oh my god"
-else:
-	invenline = inven.readlines()
+#else:
+#	invenline = inven.readline()
 
 try:
 	inf = open("loggedIn.csv", "r")
@@ -40,8 +41,12 @@ else:
 			"""
 		else:
 			#display the bill
-		for l in range(len(invenline)):
-			numitem1 = invenline.split(",")
+			for index in range(1):
+					
+				sumtot = 0
+
+				inventoryline = inven.next()
+				numitem1 = inventoryline.split(",")
 	#			for i in range(len(invenline)):
 				print "Content-type: text/html\n\n"
 				print """
@@ -57,41 +62,53 @@ else:
 				<tr>
 				"""
 				form = cgi.FieldStorage() 
-#			checkedboxes1 = requests.get('item1')
-#			print checkedboxes
+#				checkedboxes1 = requests.get('item1')
+#				print checkedboxes
 				basket = form.getvalue('item1')
-
 #			if checkedboxes1:
-				
-				if eval('int(numitem1[1]) - int(basket[0])') < 0:
-					print "<p>ERROR: PURCHASE NOT PERMITTED.</P>"
-	
-				print "<td>%s</td>" %(basket[0])
-				print "<td>%s</td>" %(basket[1])
-				cost1 = mult(int(basket[1]), 1)
-				print "<td>%d</td><tr>" %(cost1)
+
+				if eval('int(numitem1[1]) - int(basket[1])') < 0:
+					print "<p>ERROR: PURCHASE NOT PERMITTED.</p>"
+					#PUT SOME KIND OF GODDAMN QUIT SHIT HERE OR W/E
+				else:
+					print "<td>%s</td>" %(basket[0])
+					print "<td>%s</td>" %(basket[1])
+					cost1 = mult(int(basket[1]), 1)
+					newitem1inv = eval('int(numitem1[1]) - int(basket[1])')
+					print "<td>%d</td><tr>" %(cost1)
+					sumtot += cost1
 			
-#			checkedboxes2 = requests.get('item2')
+				inventoryline = inven.next()
+				numitem2 = inventoryline.split(",")			
+#				checkedboxes2 = requests.get('item2')
 				basket = form.getvalue('item2')
-
+							
+				if eval('int(numitem2[1]) - int(basket[1])') < 0:
+					print "<p>ERROR: PURCHASE NOT PERMITTED.</p>"
 #			if checkedboxes2:
-				print "<td>%s</td>" %(basket[0])
-				print "<td>%s</td>" %(basket[1])
-				cost2 = mult(int(basket[1]), 2)
-				print "<td>%d</td><tr>" %(cost2)
-
+				else:
+					print "<td>%s</td>" %(basket[0])
+					print "<td>%s</td>" %(basket[1])
+					cost2 = mult(int(basket[1]), 2)
+					print "<td>%d</td><tr>" %(cost2)
+					sumtot += cost2
 #			checkedboxes3 = requests.get('item3')
+		
+				inventoryline = inven.next()
+				numitem3 = inventoryline.split(",")
 				basket = form.getvalue('item3')
-#
+
+				if eval('int(numitem2[1]) - int(basket[1])') < 0:
+					print "<p>ERROR: PURCHASE NOT PERMITTED.</p>"
+				else:
 #			if checkedboxes3:
-				print "<td>%s</td>" %(basket[0])
-				print "<td>%s</td>" %(basket[1])
-				cost3 = mult(int(basket[1]), 3)
-				print "<td>%d</td><tr>" %(cost3)
-			
+					print "<td>%s</td>" %(basket[0])
+					print "<td>%s</td>" %(basket[1])
+					cost3 = mult(int(basket[1]), 3)
+					print "<td>%d</td><tr>" %(cost3)
+					sumtot += cost3
 				print "</tr><td></td><td></td><td>"
-				sum = eval('cost1 + cost2 + cost3')
-				print "%d</td>" %(sum)
+				print "%s</td>" %(sumtot)
 				print "</tr></table>"
 		#	while basket != None:
 			#	print "<p>%s %s</p>" %(basket[0], basket[1])
